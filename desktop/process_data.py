5841f17f93df014ff9b239123adef5b543c00d1d
Jonathan Miner <cp_n18@yahoo.com>
2019-07-02 19:03:02 -0400

    Add SPEED THRESHOLD

--- desktop/process_data.py
+++ desktop/process_data.py
@@ -5,6 +5,7 @@
 import math
 import sys
 
+SPEED_THRESHOLD=2.0
 
 
 #filename="201706052258_log_wolfeboro.csv"
@@ -83,17 +84,43 @@ def parse_accel_v4(accel, obj, a):
 # Version 5 File
 
 def parse_gps_v5(obj, a):
-    mps_to_mph = 2.23694
+    ms_to_mph = 2.23694
     m_to_ft = 3.28084
 
     obj['lat'] = float(a[2])
     obj['lon'] = float(a[3])
-    obj['alt'] = float(a[4]) * m_to_ft
-    obj['speed'] = float(a[5]) * mps_to_mph
+    if a[4] != "-":
+        obj['alt'] = float(a[4]) # * m_to_ft
+    obj['speed'] = float(a[5]) # * ms_to_mph
     obj['bearing'] = float(a[6])
     return obj
 
 def parse_accel_v5(accel, obj, a):
+    return parse_accel_v3(accel, obj, a)
+
+# Version 6 File
+
+def parse_gps_v6(obj, a):
+    ms_to_mph = 2.23694
+    m_to_ft = 3.28084
+
+    obj['lat'] = float(a[2])
+    obj['lon'] = float(a[3])
+    if a[4] != "-":
+        obj['alt'] = float(a[4]) * m_to_ft
+    else:
+        obj['alt'] = 0
+    if a[8] != "-":
+        obj['speed'] = float(a[8]) * ms_to_mph
+    else:
+        obj['speed'] = 0
+    if a[10] != "-":
+        obj['bearing'] = float(a[10])
+    else:
+        obj['bearing'] = 0
+    return obj
+
+def parse_accel_v6(accel, obj, a):
     return parse_accel_v3(accel, obj, a)
 
 def read_data(filename):
@@ -125,15 +152,19 @@ def read_data(filename):
                     obj = parse_accel_v3(accel, obj, a)
                 elif version == 4:
                     obj = parse_accel_v4(accel, obj, a)
-                else:
+                elif version == 5:
                     obj = parse_accel_v5(accel, obj, a)
+                else:
+                    obj = parse_accel_v6(accel, obj, a)
             elif obj['type'] == 'G':
                 if version == 3:
                     obj = parse_gps_v3(obj, a)
                 elif version == 4:
                     obj = parse_gps_v4(obj, a)
-                else:
+                elif version == 5:
                     obj = parse_gps_v5(obj, a)
+                else:
+                    obj = parse_gps_v6(obj, a)
                 gps[obj['time']] = obj
 
             if ok(obj):
@@ -170,7 +201,10 @@ def fill_buckets(accel):
             a['delta'] = calc_delta(a)
             #a['delta'] = a['avgx']
             count += 1
-            b = int(abs(a['delta']))
+            if a['speed'] > SPEED_THRESHOLD:
+                b = int(abs(a['delta']))
+            else:
+                b = 0
             try:
                 buckets[b] += 1
             except:
@@ -210,30 +244,36 @@ def line_chart(accel, threshold):
     y1=[]
     y2=[]
     y3=[]
+    for i in range(2, len(accel)-3):
+        max_s = max(accel[i-2]['speed'], accel[i-1]['speed'], accel[i]['speed'], accel[i+1]['speed'], accel[i+2]['speed'])
+        min_s = min(accel[i-2]['speed'], accel[i-1]['speed'], accel[i]['speed'], accel[i+1]['speed'], accel[i+2]['speed'])
+        accel[i]['speed'] = (accel[i-2]['speed'] + accel[i-1]['speed'] + accel[i]['speed'] + accel[i+1]['speed'] + accel[i+2]['speed'] - max_s - min_s) / 3
+
     for a in accel:
         if a['type'] == 'A':
-            if a['delta'] >= threshold and a['speed'] > 0:
-                y1.append(a['delta'])
-                print("%0.6f %0.6f %d" % (a['lat'], a['lon'], a['delta']))
-            else:
-                y1.append(0)
-            y2.append(a['speed'])
-            #if a['speed'] > 0:
-            #    y1.append(a['minx'])
-            #    y2.append(a['maxx'])
-            #    y3.append(a['avgx'])
+            #if a['delta'] >= threshold and a['speed'] > 0:
+            #    y1.append(a['delta'])
+            #    print("%0.6f %0.6f %d" % (a['lat'], a['lon'], a['delta']))
             #else:
             #    y1.append(0)
-            #    y2.append(0)
-            #    y3.append(0)
+            #y2.append(a['speed'])
+            if a['speed'] > SPEED_THRESHOLD:
+                y1.append(a['minx'])
+                y2.append(a['maxx'])
+                y3.append(a['avgx'])
+            else:
+                y1.append(0)
+                y2.append(0)
+                y3.append(0)
 
-    df = pd.DataFrame({'x': range(0,len(y1)), 'avg': y1, 'speed': y2})
-    plt.plot('x', 'avg', data=df, linestyle='-', marker='')
-    plt.plot('x', 'speed', data=df, linestyle='-', marker='')
-    #df = pd.DataFrame({'x': range(0,len(y1)), 'min': y1, 'max': y2, 'avg': y3})
-    #plt.plot('x', 'min', data=df, linestyle='-', marker='')
-    #plt.plot('x', 'max', data=df, linestyle='-', marker='')
+    #df = pd.DataFrame({'x': range(0,len(y1)), 'avg': y1, 'speed': y2})
     #plt.plot('x', 'avg', data=df, linestyle='-', marker='')
+    #plt.plot('x', 'speed', data=df, linestyle='-', marker='')
+    df = pd.DataFrame({'x': range(0,len(y1)), 'min': y1, 'max': y2, 'avg': y3})
+    plt.plot('x', 'min', data=df, linestyle='-', marker='')
+    plt.plot('x', 'max', data=df, linestyle='-', marker='')
+    plt.plot('x', 'avg', data=df, linestyle='-', marker='')
+
     plt.legend()
     plt.show()
 
@@ -247,7 +287,7 @@ def drawmap(accel, threshold):
         if a['type'] == 'A':
             lat.append(a['lat'])
             lon.append(a['lon'])
-            if a['delta'] >= threshold and a['speed'] > 0:
+            if a['delta'] >= threshold and a['speed'] > SPEED_THRESHOLD:
                 #s.append(10*a['delta']/a['speed'])
                 s.append(a['delta'])
                 print("%0.6f %0.6f %d" % (a['lat'], a['lon'], a['delta']))
@@ -262,5 +302,5 @@ def drawmap(accel, threshold):
     plt.title('Track')
     plt.show()
 
-#line_chart(accel, noise_floor)
+line_chart(accel, noise_floor)
 drawmap(accel, noise_floor)
