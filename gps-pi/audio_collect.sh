9b0c2d76afd7ad80c6f5d88602bff6d7953efb53
Jonathan Miner <jminer@localhost.localdomain>
2020-11-06 18:29:02 -0500

    New script to capture audio

---
+++ gps-pi/audio_collect.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+datadir=/root/gps-data
+
+collect()
+{
+  ts=`date +%Y%m%d%H%M%S`
+  arecord \
+    -d 60 \
+    --device=hw:CARD=Device,DEV=0 \
+    --format S16_LE \
+    --rate 44100 \
+    -c1 ${datadir}/${ts}_left.wav &
+  arecord \
+    -d 60 \
+    --device=hw:CARD=Device_1,DEV=0 \
+    --format S16_LE \
+    --rate 44100 \
+    -c1 ${datadir}/${ts}_right.wav &
+  wait
+}
+
+while true; do
+	collect
+done
