ae73152f43fc67a53f9fda5357b2883d05adfa85
Jonathan Miner <jminer@localhost.localdomain>
2020-11-07 13:28:34 -0500

    Fix name of audio collect script

--- gps-pi/log.sh
+++ gps-pi/log.sh
@@ -13,7 +13,7 @@ fi
 mii-tool eth0 >> ${output}/error.log
 
 #if [ "`mii-tool eth0`" == "eth0: no link" ]; then
-  ./audio_capture.sh &
+  ./audio_collect.sh &
   while true; do
     date +%Y%m%d%H%M > ${output}/timestamp
     ./combolog9.py \
