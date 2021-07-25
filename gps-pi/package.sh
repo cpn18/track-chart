84ed3be204620a5e06631c400995626970489701
Jonathan Miner <cp_n18@yahoo.com>
2021-07-25 06:26:45 -0400

    Added Packager

---
+++ gps-pi/package.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+hash=$(git describe --tags)
+
+files="adxl345.py \
+    adxl345_shim.py \
+    berryimu_shim.py \
+    config.json.sample \
+    gps_logger.py \
+    gps_logger.sh \
+    imu_logger.py \
+    imu_logger.sh \
+    IMU.py \
+    launcher.py \
+    lidar_logger.py \
+    lidar_logger.sh \
+    log.sh \
+    lpcm_collect.sh \
+    lpcm_logger.py \
+    lpcm_logger.sh \
+    LSM9DS0.py \
+    LSM9DS1.py \
+    nmea.py \
+    pirail-mini.py \
+    wait_for_gps_fix.py \
+    web_server.py \
+    web_server.sh \
+    htdocs \
+    js \
+    os_setup"
+
+echo $hash > version.txt
+tar -zcf ${HOME}/PiRail-${hash}.tgz ${files} version.txt
+rm version.txt
