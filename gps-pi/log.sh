f43552adc4ef89d8451d2d3b31af42beefb96670
Jonathan Miner <cp_n18@yahoo.com>
2022-04-19 20:08:30 -0400

    Create Mount Point

--- gps-pi/log.sh
+++ gps-pi/log.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 config="config.json"
+usb_drive="/dev/sda1"
+mount_point="/media/usb0"
+pirail_data=${mount_point}/PIRAIL
 
 cd `dirname $0`
 
-mount /dev/sda1 /media/usb0
+if [ ! -d ${mount_point} ]; then
+  mkdir -p ${mount_point}
+fi
+
+mount ${usb_drive} ${mount_point}
 
 ./wait_for_gps_fix.py
 
-if [ -d /media/usb0/PIRAIL ]; then
-  output=/media/usb0/PIRAIL/`date +%Y%m%d`
+if [ -d ${pirail_data} ]; then
+  output=${pirail_data}/`date +%Y%m%d`
 else
   output=/root/gps-data
 fi
