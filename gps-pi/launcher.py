0136b2dee9d70d942909797b11e5e6fd8140176c
Jonathan Miner <cp_n18@yahoo.com>
2022-05-01 11:22:19 -0400

    Refactor

--- gps-pi/launcher.py
+++ gps-pi/launcher.py
@@ -7,25 +7,26 @@ import os
 
 import util
 
-def launch(task, output_directory):
+def launch(task):
+    """ Launch a command """
+    path = os.path.join(os.getcwd(), task['cmd'][0])
+    args = task['cmd']
+    for i in range(len(args)):
+        if args[i] == "%OUTPUT_DIR%":
+            args[i] = OUTPUT
+        elif args[i] == "%PORT%":
+            args[i] = str(task['port'])
+    #print(path, args)
+
     pid = os.fork()
     if pid != 0: # Parent
         pass
     else:        # Child
-        path = os.path.join(os.getcwd(), task['cmd'][0])
-        args = task['cmd']
-
-        for i in range(len(args)):
-            if args[i] == "%OUTPUT_DIR%":
-                args[i] = output_directory
-            elif args[i] == "%PORT%":
-                args[i] = str(task['port'])
-
-        #print(path, args)
         os.execv(path, args)
         sys.exit(-1)
 
-def launcher(output_directory):
+def launcher():
+    """ Launch all commands """
     config = util.read_config()
 
     for task in config:
@@ -33,7 +34,7 @@ def launcher(output_directory):
             if not 'enable' in config[task]:
                 config[task]['enable'] = True
             if config[task]['enable']:
-                launch(config[task], output_directory)
+                launch(config[task])
 
 if __name__ == "__main__":
     try:
@@ -41,4 +42,4 @@ if __name__ == "__main__":
     except IndexError:
         OUTPUT = "/root/gps-data"
 
-    launcher(OUTPUT)
+    launcher()
