56198808271514a090e9f3238edaf42930b833d8
Jonathan Miner <cp_n18@yahoo.com>
2022-09-24 17:26:04 -0400

    Refactor for Performance

--- desktop/03_add_mileage_from_json.py
+++ desktop/03_add_mileage_from_json.py
@@ -1,115 +1,89 @@
 #!/usr/bin/env python3
+"""
+Add Mileage and GPS locations to all objects
+"""
 import os
 import json
 import sys
-import geo
 import gps_to_mileage
-import datetime
 
 import pirail
 
-if len(sys.argv) < 3:
-    print("USAGE: %s [args] json_file known_file" % sys.argv[0])
-    sys.exit(1)
+# Calculate mileage from GPS location
+GET_MILEAGE_FROM_GPS = False
 
-json_file = os.path.abspath(sys.argv[-2])
-known_file = sys.argv[-1]
+def add_mileage_and_gps(json_file, known_file, output_file):
+    """
+    Add GPS and mileage data to all elements
+    """
+    acclist = []
+    last_tpv = {}
+    gps = gps_to_mileage.Gps2Miles(known_file)
+    with open(output_file, "w") as output:
+        for _line_no, obj in pirail.read(json_file):
+            if obj['class'] in ["SKY", "ATT", "LIDAR", "LPCM"]:
+                acclist.append(obj)
 
-GPS = gps_to_mileage.Gps2Miles(known_file)
-
-acclist = []
-data = []
-last_tpv = None
-
-for line_no, obj in pirail.read(json_file):
-    if obj['class'] in ["SKY", "ATT", "LIDAR", "LPCM"]:
-        acclist.append(obj)
-    elif obj['class'] == "TPV":
-        if last_tpv is not None:
-            if obj['time'] == last_tpv['time']:
+            if obj['class'] != "TPV":
                 continue
-            time_start = pirail.parse_time(obj['time'])
-            time_delta = time_start - pirail.parse_time(last_tpv['time'])
-            if len(acclist) > 0:
-                delta_lat = (obj['lat'] - last_tpv['lat'])
-                delta_lon = (obj['lon'] - last_tpv['lon'])
-                delta_alt = (obj['alt'] - last_tpv['alt'])
-                #print ("delta_lat", delta_lat)
-                #print ("delta_lon", delta_lon)
-                #print ("time_delta", time_delta.total_seconds())
-                for acc in acclist:
-                    acc_time = pirail.parse_time(acc['time'])
 
-                    millsec = (acc_time - time_start).total_seconds() / time_delta.total_seconds()
+            obj['mileage'], obj['certainty'] = gps.find_mileage(obj['lat'], obj['lon'])
+            if len(last_tpv) != 0:
+                if obj['time'] == last_tpv['time']:
+                    continue
+                time_start = pirail.parse_time_in_seconds(obj['time'])
+                time_delta = time_start - pirail.parse_time_in_seconds(last_tpv['time'])
+                if len(acclist) > 0:
+                    delta_mileage = (obj['mileage'] - last_tpv['mileage'])
+                    delta_lat = (obj['lat'] - last_tpv['lat'])
+                    delta_lon = (obj['lon'] - last_tpv['lon'])
+                    try:
+                        delta_alt = (obj['alt'] - last_tpv['alt'])
+                    except KeyError:
+                        delta_alt = 0
+                    #print ("delta_lat", delta_lat)
+                    #print ("delta_lon", delta_lon)
+                    #print ("time_delta", time_delta.total_seconds())
+                    for acc in acclist:
+                        acc_time = pirail.parse_time_in_seconds(acc['time'])
 
-                    acc['lat'] = last_tpv['lat'] + millsec*delta_lat
-                    acc['lon'] = last_tpv['lon'] + millsec*delta_lon
-                    acc['alt'] = last_tpv['alt'] + millsec*delta_alt
-                    acc['mileage'], acc['certainty'] = GPS.find_mileage(acc['lat'], acc['lon'])
-                    data.append(acc)
-        obj['mileage'], obj['certainty'] = GPS.find_mileage(obj['lat'], obj['lon'])
-        data.append(obj)
-        last_tpv = obj
-        acclist = []
+                        millsec = (acc_time - time_start) / time_delta
 
-print("Leftover elements = %d" % len(acclist))
+                        acc['lat'] = last_tpv['lat'] + millsec*delta_lat
+                        acc['lon'] = last_tpv['lon'] + millsec*delta_lon
+                        acc['alt'] = last_tpv['alt'] + millsec*delta_alt
 
-# Sort By
-#sortby='mileage'
-SORTBY='time'
-if SORTBY != 'time':
-    data = sorted(data, key=lambda k: k[SORTBY], reverse=False)
+                        if GET_MILEAGE_FROM_GPS:
+                            acc['mileage'], acc['certainty'] = gps.find_mileage(acc['lat'], acc['lon'])
+                        else:
+                            acc['mileage'] = last_tpv['mileage'] + millsec*delta_mileage
+                            acc['certainty'] = obj['certainty'] * last_tpv['certainty']
 
-output_filename, output_extension = json_file.split('.',1)
-output_file = "%s_with_mileage_sort_by_%s.%s" % (
-    output_filename,
-    SORTBY,
-    output_extension,
-)
+                        output.write(json.dumps(acc))
+                        output.write("\n")
 
-pirail.write(output_file, data)
+            output.write(json.dumps(obj))
+            output.write("\n")
+            last_tpv = obj
+            acclist = []
 
-new_data = []
-accset = []
+    print("Leftover elements = %d" % len(acclist))
 
-# Find Starting Mileage
-current = 0
-step = 0.001
-while True:
-    if not 'mileage' in data[current]:
-        current += 1
-    else:
-        start_mileage = round(data[current]['mileage'],2)
-        next_mileage = start_mileage + step
-        break
+def get_output_filename(input_file):
+    """ Build Outout Filename """
+    output_filename, output_extension = input_file.split('.',1)
+    return "%s_with_mileage_sort_by_time.%s" % (
+        output_filename,
+        output_extension,
+    )
 
-while current < len(data):
-    if not 'mileage' in data[current]:
-        current += 1
-        continue
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("USAGE: %s [args] json_file known_file" % sys.argv[0])
+        sys.exit(1)
 
-    if round(data[current]['mileage'],2) < next_mileage:
-        accset.append(data[current])
-        current += 1
-    else:
-        acc_z = -9999
-        for a in accset:
-            if a['class'] != "ATT":
-                continue
-            if a['acc_z'] > acc_z:
-                acc_z = a['acc_z']
-        if acc_z != -9999:
-            new_data.append({'class': "ATT", 'mileage': start_mileage, 'acc_z': acc_z, 'lat': data[current]['lat'], 'lon': data[current]['lon']})
-        accset = []
-        start_mileage = next_mileage
-        next_mileage += step
+    INPUT_FILE = os.path.abspath(sys.argv[-2])
+    KNOWN_FILE = sys.argv[-1]
+    OUTPUT_FILE = get_output_filename(INPUT_FILE)
 
-with open("acc_by_mileage.json", "w") as f:
-    for obj in new_data:
-        f.write(json.dumps(obj)+"\n")
-
-with open("acc_by_mileage.csv", "w") as f:
-    f.write("Mileage Latitude Longitude ACCz\n")
-    for d in new_data:
-        if d['class'] == "ATT":
-            f.write("%0.3f %f %f %f\n" % (d['mileage'],d['lat'], d['lon'], d['acc_z']))
+    add_mileage_and_gps(INPUT_FILE, KNOWN_FILE, OUTPUT_FILE)
