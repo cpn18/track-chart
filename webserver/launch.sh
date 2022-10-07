7a96e2a981942f5092832c1d663171dd746879cc
Jonathan Miner <cp_n18@yahoo.com>
2022-10-07 05:29:11 -0400

    Workaround for not having external drive

--- webserver/launch.sh
+++ webserver/launch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 export PYTHONPATH=../desktop
-export PIRAILDATA="/run/media/jminer/FreeAgent Drive/PIRAIL/20210725"
+
+# Check for external storage - WORKAROUND
+if [ -d "/run/media/jminer/FreeAgent Drive" ]; then
+    export PIRAILDATA="/run/media/jminer/FreeAgent Drive/PIRAIL/20210725"
+fi
 
 python web_server.py 8080
