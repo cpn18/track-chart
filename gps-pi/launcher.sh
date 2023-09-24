3fbaf4511a5a10eca6e84c8230c6bf22d7d38bee
Jonathan Miner <cp_n18@yahoo.com>
2023-09-24 18:31:53 -0400

    Rename log.sh to launcher.sh

--- gps-pi/launcher.sh
+++ gps-pi/launcher.sh
@@ -7,6 +7,7 @@ pirail_data=${mount_point}/PIRAIL
 
 cd `dirname $0`
 
+# Try to mount the USB drive
 if [ -b ${usb_drive} ]; then
   if [ ! -d ${mount_point} ]; then
     mkdir -p ${mount_point}
@@ -15,18 +16,26 @@ if [ -b ${usb_drive} ]; then
   mount ${usb_drive} ${mount_point}
 fi
 
-./wait_for_gps_fix.py
-
+# If the PiRail Data directory is found, use it
 if [ -d ${pirail_data} ]; then
-  output=${pirail_data}/`date +%Y%m%d`
+  output=${pirail_data}
 else
   output=/root/gps-data
 fi
 
+# Add a date stamp
+output=${output}/`date +%Y%m%d`
+
+# Create the directory if needed
 mkdir -p ${output}
 
+# If the configuration file doesn't exist, copy the sample
 if [ ! -f ${config} ]; then
   cp ${config}.sample ${config}
 fi
 
+# Wait for a GPS fix... this can be troublesome with poor GPS coverage
+./wait_for_gps_fix.py
+
+# Launch everything else
 ./launcher.py ${output}
